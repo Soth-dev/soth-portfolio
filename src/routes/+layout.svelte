06f6bbbb8439f5ad2fb131c8feff1fcaa0e45927
Soth-dev <mcbedrockplayer2008@gmail.com>
2026-05-17 08:16:52 +0700

    add header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Header from '$lib/components/Header.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<Header></Header>
+<div style="display: contents">
+	{@render children()}
+	<h1>testttt</h1>
+</div>
